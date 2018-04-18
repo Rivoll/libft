d044449bb06b9968369b248fb15739b24b65e5d2
Patrick Keita <patrick9@live.fr>
2018-04-18 03:48:33 +0200

    Update ft_atlli_base.c

--- ft_atlli_base.c
+++ ft_atlli_base.c
@@ -39,33 +39,3 @@ long long int		ft_atlli_base(char *nb, int base_nb)
 	}
 	return (result * sign);
 }
-
-/*
-long long int		ft_atllin_base(char *nb, int base_nb, int n)
-{
-	long long int	result;
-	int				sign;
-	int				i;
-
-	if (base_nb > 16 || base_nb < 2)
-		return (-1);
-	sign = 1;
-	i = 0;
-	result = 0;
-	nb[i] == '+' ? i++ : 0;
-	nb[i] == '-' ? sign = -1 : 0;
-	nb[i] == '-' ? i++ : 0;
-	while (nb[i] && i < n)
-	{
-		if (nb[i] >= '0' && nb[i] <= '9')
-			result = result * base_nb + nb[i++] - '0';
-		else if (nb[i] >= 'a' && nb[i] <= 'f')
-			result = result * base_nb - 87 + nb[i++];
-		else if (nb[i] >= 'A' && nb[i] <= 'F')
-			result = result * base_nb - 55 + nb[i++];
-		else
-			break ;
-	}
-	return (result * sign);
-}
-*/
